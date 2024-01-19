929d493917d43891079c7719ab58ab179fb5c492
Daniel Phan <phand972@gmail.com>
2024-01-18 19:34:03 -0600

    Parameterizing inputs

--- src/commands/utilities/submit.ts
+++ src/commands/utilities/submit.ts
@@ -13,34 +13,39 @@ module.exports = {
       option.setName('tags')
         .setDescription('Comma separated tags for the art piece.')),
   async execute (interaction: any, dbClient: Client) {
-    // TODO: Add validations and sanitize
     try {
       const submitter = interaction.user.username
       const externalLink = getTransformedLink(new URL(interaction.options.getString('link'))) // eslint-disable-line @typescript-eslint/no-unsafe-argument
       const tags = interaction.options.getString('tags')
       const createdOn = new Date().toISOString()
 
-      const existing = await dbClient.query(`SELECT discord_link FROM public.submissions WHERE external_link = '${externalLink}'`)
+      const existing = await dbClient.query('SELECT discord_link FROM public.submissions WHERE external_link = $1', [`${externalLink}`])
       if (existing.rowCount! > 0) {
         throw new Error(`This link has already been submitted: ${existing.rows[0].discord_link}`)
       }
 
       // Send a reply and retrieve the link
       const reply = await interaction.reply({
-        content: `${externalLink} - ${tags}`,
+        content: tags ? `${externalLink} - ${tags}` : externalLink,
         fetchReply: true
       })
       const discordLink = `https://discord.com/channels/${reply.guildId}/${reply.channelId}/${reply.id}`
 
       // Insert into submissions and submission_tags tables
       await dbClient.query('BEGIN')
-      const submission = await dbClient.query(`INSERT INTO public.submissions(submitter, external_link, discord_link, created_on, updated_on) VALUES ('${submitter}', '${externalLink}', '${discordLink}', '${createdOn}', '${createdOn}') RETURNING id`)
 
-      const tagArray = tags.split(',')
+      const submission = await dbClient.query('INSERT INTO public.submissions(submitter, external_link, discord_link, created_on, updated_on) VALUES ($1, $2, $3, $4, $5) RETURNING id',
+        [`${submitter}`, `${externalLink}`, `${discordLink}`, `${createdOn}`, `${createdOn}`]
+      )
+
+      const tagArray = tags.split(',') ?? []
       tagArray.forEach(async (tag: string) => {
         tag = tag.trim().toLowerCase()
-        await dbClient.query(`INSERT INTO public.submission_tags(submission_id, tag) VALUES('${submission.rows[0].id}', '${tag}')`)
+        await dbClient.query('INSERT INTO public.submission_tags(submission_id, tag) VALUES($1, $2)',
+          [`${submission.rows[0].id}`, `${tag}`]
+        )
       })
+
       await dbClient.query('COMMIT')
     } catch (e) {
       console.log(e)
@@ -59,5 +64,5 @@ const getTransformedLink = (originalUrl: URL): string => {
   if (originalUrl.host === 'x.com') {
     link = originalUrl.href.replace('x.com', 'vxtwitter.com')
   }
-  return link
+  return link.trim()
 }
