0ab3e87207f845ddb6372e0e6235cbef67ae6d2f
Daniel Phan <phand972@gmail.com>
2025-02-15 15:29:54 -0600

    Making spoiler list human readable

--- src/services/commandHandlers/buildMessage.ts
+++ src/services/commandHandlers/buildMessage.ts
@@ -3,10 +3,32 @@ import { spoiler } from 'discord.js'
 export const buildMessage = (link: string, tags: string[] = [], spoilers: string[] = []): string => {
   let message = ''
   if (spoilers.length > 0) {
-    message += `Spoilers for ${spoilers.join(', ')} - ${spoiler(link + (tags.length > 0 ? ' - ' + tags.join(', ') : ''))} `
+    message += `Spoilers for ${buildSpoilers(spoilers)} - ${spoiler(link + (tags.length > 0 ? ' - ' + tags.join(', ') : ''))} `
   } else {
     message += `${link}${(tags.length > 0 ? ' - ' + tags.join(', ') : '')}`
   }
 
   return message
+}
+
+const buildSpoilers = (spoilers: string[]): string => {
+  if (spoilers.length === 0) {
+    return ''
+  }
+  if (spoilers.length === 1) {
+    return `'${spoilers[0]}'`
+  }
+  if (spoilers.length === 2) {
+    return `'${spoilers[0]}' and '${spoilers[1]}'`
+  }
+
+  let result = ''
+  spoilers.forEach((s, i) => {
+    if (i !== spoilers.length - 1) {
+      result += `'${s}', `
+    } else {
+      result += `and '${s}'`
+    }
+  })
+  return result
 }
